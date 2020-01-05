395de4eccada1079e32492b566b1b1f6e76fcad2
Romilly Cocking <romilly@gmail.com>
2020-01-05 14:43:31 +0000

    revised toutch-03; moved lled code to traffic-lights

--- experiments/touch/led-touch-03.py
+++ experiments/touch/led-touch-03.py
@@ -3,17 +3,13 @@ from time import sleep
 
 
 while True:
+    explorerhat.light.off()
     if explorerhat.touch.one.is_pressed():
-        explorerhat.output.one.on()
-    else:
-        explorerhat.output.one.off()
+        explorerhat.light.blue.on()
     if explorerhat.touch.two.is_pressed():
-        explorerhat.output.two.on()
-    else:
-        explorerhat.output.two.off()
+        explorerhat.light.yellow.on()
     if explorerhat.touch.three.is_pressed():
-        explorerhat.output.three.on()
-    else:
-        explorerhat.output.three.off()
+        explorerhat.light.red.on()
+    if explorerhat.touch.four.is_pressed():
+        explorerhat.light.green.on()
     sleep(0.1)
-
