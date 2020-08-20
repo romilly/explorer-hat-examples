195edcf90a7bc770cd2198277c0568929ec1aaa7
Romilly Cocking <romilly.cocking@gmail.com>
2020-08-20 13:18:04 +0100

    Buzzing

---
+++ experiments/buzzer/buzzer.py
@@ -0,0 +1,19 @@
+import explorerhat as eh
+import RPi.GPIO as GPIO
+
+
+## Sets up GPIO pin 18 as a PWM output with freq. of 400 Hz.
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
+buzzer = GPIO.PWM(18, 400)
+
+while True:
+    if eh.touch.one.is_pressed():
+        buzzer.start()
+    else:
+        buzzer.stop()
+
+
+
+
