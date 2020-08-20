0c5473c98091d691bd4bcf3d63e8d57f4665f56f
Romilly Cocking <romilly.cocking@gmail.com>
2020-08-20 14:53:11 +0100

    Traffic Lights v1.

--- experiments/traffic-lights/traffic-lights-01.py
+++ experiments/traffic-lights/traffic-lights-01.py
@@ -1,32 +1,26 @@
-from collections import namedtuple
-
-import explorerhat
+import explorerhat as eh
 from time import sleep
 
 
-RED = explorerhat.output.one
-AMBER = explorerhat.output.two
-GREEN = explorerhat.output.three
-LIGHTS = [RED, AMBER, GREEN]
+RED = eh.lights.red
+AMBER = eh.lights.yellow
+GREEN = eh.lights.green
+ALL_LIGHTS = [RED, AMBER, GREEN]
 
-State = namedtuple('State', ['lights','duration', 'next'])
+def show(duration, lights):
+    for light in ALL_LIGHTS:
+        light.off()
+    for light in lights:
+        light.on()
+    sleep(duration)
 
-STATES = {
-    'stop':     State([RED,], 5, 'ready'),
-    'ready':    State([AMBER], 1, 'go'),
-    'go':       State([GREEN], 5, 'caution'),
-    'caution':  State([RED, AMBER], 1, 'stop')
-}
-
-state = STATES['stop']
 
 while True:
-    for light in LIGHTS:
-        light.off()
-    for light in state.lights:
-        light.on()
-    sleep(state.duration)
-    state = STATES[state.next]
+    show(5, [RED])
+    show(1, [AMBER])
+    show(5,[GREEN])
+    show(1, [RED, AMBER])
+
 
 
 
