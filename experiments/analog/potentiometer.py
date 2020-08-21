99f977298ac334e56c71d2e023891399a42d8e35
Romilly Cocking <romilly.cocking@gmail.com>
2020-08-21 11:15:13 +0100

    Updated potentiometer

--- experiments/analog/potentiometer.py
+++ experiments/analog/potentiometer.py
@@ -1,5 +1,7 @@
-import explorerhat
+import explorerhat as eh
+from time import sleep
 
 while True:
-    voltage = explorerhat.analog.one.read()
+    voltage = eh.analog.one.read()
     print('voltage is %f4.2 V' % voltage)
+    sleep(1)
