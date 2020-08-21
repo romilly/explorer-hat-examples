2f32d392d4752fd63245b8a6a563da13c2fedfd2
Romilly Cocking <romilly.cocking@gmail.com>
2020-08-21 11:50:52 +0100

    LDR

--- experiments/analog/ldr.py
+++ experiments/analog/ldr.py
@@ -3,12 +3,13 @@ from time import sleep
 
 while True:
     voltage = eh.analog.one.read()
-    if voltage > 2.5:
-        eh.light.red.on()
-        eh.light.green.off()
-    else:
-        eh.light.red.off()
-        eh.light.green.on()
+    print('voltage is %f4.2 V' % voltage)
+    # if voltage > 2.5:
+    #     eh.light.red.on()
+    #     eh.light.green.off()
+    # else:
+    #     eh.light.red.off()
+    #     eh.light.green.on()
     sleep(0.1)
 
 
