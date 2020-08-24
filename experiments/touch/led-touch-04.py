8204b3e0e18000fb2fbabdb26440b00a53f4ee66
Romilly Cocking <romilly.cocking@gmail.com>
2020-08-24 12:53:53 +0100

    fix import

--- experiments/touch/led-touch-04.py
+++ experiments/touch/led-touch-04.py
@@ -1,11 +1,11 @@
-import explorerhat
+import explorerhat as eh
 from time import sleep
 
 
 while True:
     for i in range(4):
-        if explorerhat.touch[i].is_pressed():
-            explorerhat.light[i].on()
+        if eh.touch[i].is_pressed():
+            eh.light[i].on()
         else:
-            explorerhat.light[i].off()
+            eh.light[i].off()
     sleep(0.1)
