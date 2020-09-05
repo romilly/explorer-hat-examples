99db6082b23eb81360f9dd94635a42248fb0469b
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-05 10:29:25 +0100

    Adding weather

--- projects/analog/weather.py
+++ projects/analog/weather.py
@@ -5,10 +5,11 @@ while True:
     voltage = eh.analog.one.read()
     centigrade = 100 * (voltage - 0.5)
     farenheit = 32 + 9 * centigrade / 5.0
+    print(voltage)
     print('temperature is %4.1f degrees C or %4.1f degrees F'
           % (centigrade, farenheit))
-    v2 = eh.analog.two.read()
-    print(v2)
-    light = 'low'  if v2 > 3.5 else 'high'
-    print('light level is %s' % light)
+    # v2 = eh.analog.two.read()
+    # print(v2)
+    # light = 'low'  if v2 > 3.5 else 'high'
+    # print('light level is %s' % light)
     sleep(1)
