0eef96a3ddaecc1e5808cf3a2c32e524474cffca
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-05 10:11:52 +0100

    Fixing analog print statement

--- projects/analog/TMP36.py
+++ projects/analog/TMP36.py
@@ -5,6 +5,6 @@ while True:
     voltage = eh.analog.one.read()
     centigrade = 100 * (voltage - 0.5)
     farenheit = 32 + 9 * centigrade / 5.0
-    print('temperature is %f4.1 degrees C or %f4.1 degrees F'
+    print('temperature is %4.1f degrees C or %4.1f degrees F'
           % (centigrade, farenheit))
     sleep(1)
