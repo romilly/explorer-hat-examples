4bf48df11400cff02807f6eb18f6a74217de1206
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-05 11:10:18 +0100

    Adding weather

--- projects/analog/weather.py
+++ projects/analog/weather.py
@@ -2,14 +2,12 @@ import explorerhat as eh
 from time import sleep
 
 while True:
-    voltage = eh.analog.two.read()
-    centigrade = 100.0 * (voltage - 0.5)
+    v1 = eh.analog.two.read()
+    centigrade = 100.0 * (v1 - 0.5)
     farenheit = 32 + 9 * centigrade / 5.0
-    print(voltage)
     print('temperature is %4.1f degrees C or %4.1f degrees F'
           % (centigrade, farenheit))
-    # v2 = eh.analog.two.read()
-    # print(v2)
-    # light = 'low'  if v2 > 3.5 else 'high'
-    # print('light level is %s' % light)
+    v2 = eh.analog.two.read()
+    light = 'low'  if v2 > 3.5 else 'high'
+    print('light level is %s' % light)
     sleep(1)
