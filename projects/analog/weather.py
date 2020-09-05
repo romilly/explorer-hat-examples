de6eaa62ec844ead2732cb909b429b1e12f6f18b
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-05 11:10:59 +0100

    Adding weather

--- projects/analog/weather.py
+++ projects/analog/weather.py
@@ -2,7 +2,7 @@ import explorerhat as eh
 from time import sleep
 
 while True:
-    v1 = eh.analog.two.read()
+    v1 = eh.analog.one.read()
     centigrade = 100.0 * (v1 - 0.5)
     farenheit = 32 + 9 * centigrade / 5.0
     print('temperature is %4.1f degrees C or %4.1f degrees F'
