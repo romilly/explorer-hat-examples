33279b6514bfb2d93d2b03a26456cc74e7bdb502
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-05 10:49:04 +0100

    Adding weather

--- projects/analog/weather.py
+++ projects/analog/weather.py
@@ -3,7 +3,7 @@ from time import sleep
 
 while True:
     voltage = eh.analog.one.read()
-    centigrade = 100 * (voltage - 0.5)
+    centigrade = 100.0 * (voltage - 0.5)
     farenheit = 32 + 9 * centigrade / 5.0
     print(voltage)
     print('temperature is %4.1f degrees C or %4.1f degrees F'
