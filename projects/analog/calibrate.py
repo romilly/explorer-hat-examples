b8871fe52f533517215bf6540371776434ea059e
Romilly Cocking <romilly.cocking@gmail.com>
2020-09-08 09:17:47 +0100

    added calibrate.py

--- projects/analog/calibrate.py
+++ projects/analog/calibrate.py
@@ -4,6 +4,7 @@ from time import sleep
 
 while True:
     v2 = eh.analog.two.read()
-    light_level = 1000 * v2 / (5 - v2)
-    print('light level is %4.1f' % light_level)
+    r1 = 10000
+    light_level = r1 * v2 / (5 - v2)
+    print('v2 is %4.1f andlight level is %4.1f' % (v2, light_level))
     sleep(1)
